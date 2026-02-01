45c99eb2d6ac9e5b2d4796936e09fc99a7d3a02a
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-02-01 12:56:13 +0600

    Create Logo Section

---
+++ src/pages/shared/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logoImg from '../../assets/cooking.png'
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+    return (
+        <Link to={'/'} className="flex items-center space-x-2">
+            {/* icon */}
+            <img className='w-10 h-10 object-cover' src={logoImg} alt="logo-img" />
+
+            {/* text logo */}
+            <div className="font-bold text-4xl">
+                <span className="text-gray-100">Food</span>
+                <span className="text-[#7efff5]">Order</span>
+            </div>
+        </Link>
+    );
+};
+
+export default Logo;
